1430442e712d28737be802e3a567b15609dc487b
kholostyak17 <kholostyak17@gmail.com>
2021-04-30 11:11:56 +0000

    complete

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,16 +8,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="d-flex justify-content-center">
-			<div className="digit m-2 px-4">
+			<div className="digit m-2">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="digit m-2">
 				<SecondsCounter time={1000} />
 			</div>
-			<div className="digit m-2 px-4">
+			<div className="digit m-2">
 				<SecondsCounter time={100} />
 			</div>
-			<div className="digit m-2 px-4">
+			<div className="digit m-2">
 				<SecondsCounter time={10} />
 			</div>
-			<div className="digit m-2 px-4">
+			<div className="digit m-2">
 				<SecondsCounter time={1} />
 			</div>
 		</div>
